7ae86630133e360bb6d8fd08b8663193887e8444
Cesar Choccata <c21choccata@gmail.com>
2024-07-07 13:27:03 -0500

    Add portfolio module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundPageComponent } from './shared/pages/not-found-page/not-found-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'portfolio-cesar',
+    loadChildren:()=>import('./portfolio/portfolio.module').then(m=>m.PortfolioModule)
+  },
+  {
+    path:'',
+    redirectTo: 'portfolio-cesar',
+    pathMatch:'full'
+  },
+  {
+    path:'404',
+    component: NotFoundPageComponent
+  },
+  {
+    path:'**',
+    redirectTo: '404'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
