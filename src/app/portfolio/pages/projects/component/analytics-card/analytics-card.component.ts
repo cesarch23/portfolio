0627141879f36a00c85047e462876206988734db
Cesar Choccata <c21choccata@gmail.com>
2026-01-07 19:13:06 -0500

    feat: add analytics card component with Power BI iframe integration

---
+++ src/app/portfolio/pages/projects/component/analytics-card/analytics-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-analytics-card',
+  templateUrl: './analytics-card.component.html',
+  styleUrls: ['./analytics-card.component.css']
+})
+export class AnalyticsCardComponent {
+
+}
